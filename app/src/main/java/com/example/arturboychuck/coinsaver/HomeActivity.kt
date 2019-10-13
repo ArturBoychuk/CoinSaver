67836b7ac2862e65cf32d52576aa919b6e8bd9f2
artur.boychuk93@gmail.com <artur.boychuk93@gmail.com>
2019-10-13 19:27:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/arturboychuck/coinsaver/HomeActivity.kt
@@ -0,0 +1,29 @@
+package com.example.arturboychuck.coinsaver
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
